a987fdfbf050707853fff9cd38681a9955f4c87a
Philip Damra <pdamra@dealerinspire.com>
2021-10-08 21:25:44 -0700

    Typeorm configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RecipesModule } from './recipes/recipes.module';
 
 @Module({
-  imports: [RecipesModule],
+  imports: [TypeOrmModule.forRoot(), RecipesModule],
   controllers: [AppController],
   providers: [AppService],
 })
